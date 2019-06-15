afb694722e8c2884645b387e45a395889c34e782
romes <rodrigo.m.mesquita@gmail.com>
2019-06-15 22:50:51 +0100

    f

---
+++ redapplesauce.github.io/precache-manifest.16b5c6e54bec3301156373f7fb6cf531.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "766aa29207083244843a",
+    "url": "/css/about.0b8e52a5.css"
+  },
+  {
+    "revision": "766aa29207083244843a",
+    "url": "/js/about.ca69b279.js"
+  },
+  {
+    "revision": "f299c9730f9b3f3280cd",
+    "url": "/css/app.e885d29d.css"
+  },
+  {
+    "revision": "f299c9730f9b3f3280cd",
+    "url": "/js/app.6da8316d.js"
+  },
+  {
+    "revision": "cdd29a508e2b805eb443",
+    "url": "/css/chunk-55f77015.b5fcfff6.css"
+  },
+  {
+    "revision": "cdd29a508e2b805eb443",
+    "url": "/js/chunk-55f77015.2d294064.js"
+  },
+  {
+    "revision": "a278bb0b80eb53195e9f",
+    "url": "/css/chunk-63d70596.85f6e2b1.css"
+  },
+  {
+    "revision": "a278bb0b80eb53195e9f",
+    "url": "/js/chunk-63d70596.17e9b4c1.js"
+  },
+  {
+    "revision": "3cabe86e05a5ba7c9b16",
+    "url": "/css/chunk-6f98d68a.61423987.css"
+  },
+  {
+    "revision": "3cabe86e05a5ba7c9b16",
+    "url": "/js/chunk-6f98d68a.2f08d763.js"
+  },
+  {
+    "revision": "0ba51052ddc5afdc6280",
+    "url": "/js/chunk-vendors.c547bcc6.js"
+  },
+  {
+    "revision": "94d98269a2ed1d1f7cd417e9e7e41463",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
